d015f01eee09fafacf1b68826bbed85c7583e3e5
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-02-23 13:43:39 +0300

    add final commit

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ document.getElementById('result_p_v').onclick = function() {
     var mm_m = 12;
     var result = med_p + ((h_h / mm_m) * mm_p);
 
-    document.getElementById('out').innerHTML = "Давление = " + result + "Па"
+    document.getElementById('out').innerHTML = "Давление = " + result + " Па"
 };
 
 document.getElementById('result_p_g').onclick = function() {
@@ -15,7 +15,7 @@ document.getElementById('result_p_g').onclick = function() {
     var mm_m = 12;
     var result = med_p - ((h_h / mm_m) * mm_p);
 
-    document.getElementById('out').innerHTML = "Давление = " + result + "Па"
+    document.getElementById('out').innerHTML = "Давление = " + result + " Па"
 };
 
 document.getElementById('result_h').onclick = function() {
@@ -23,5 +23,23 @@ document.getElementById('result_h').onclick = function() {
     var mm_m = 12;
     var result = mm * mm_m;
 
-    document.getElementById('out').innerHTML = "Изменения на :" + result + "метров"
+    document.getElementById('out').innerHTML = "Изменения на :" + result + " метров"
+};
+
+document.getElementById('result_mm_v').onclick = function() {
+    var h = document.getElementById('h_h').value;
+    var mm_m = 12;
+    var med_mm_m = 760
+    var result = med_mm_m + (h / mm_m);
+
+    document.getElementById('out').innerHTML = result + ' миллиметров ртутного столба'
+};
+
+document.getElementById('result_mm_g').onclick = function() {
+    var h = document.getElementById('h_h').value;
+    var mm_m = 12;
+    var med_mm_m = 760
+    var result = med_mm_m - (h / mm_m);
+
+    document.getElementById('out').innerHTML = result + ' миллиметров ртутного столба'
 };
